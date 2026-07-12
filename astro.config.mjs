59ace42090dab2468f905162e2b7306f3af1a937
mahush <info@mahush.de>
2026-07-12 16:34:23 +0200

    add sidemap xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import { remarkRewriteLinks } from "./src/remark-rewrite-links.mjs";
 
 export default defineConfig({
   site: "https://funkyposts.dev",
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkRewriteLinks],
   },
